93ca126fc1bed5b66dc59b4af7815200bbc3262d
heoiu87 <brian_2447@hotmail.com>
2013-11-10 12:24:43 -0600

    Modify postNewVaccination.php

--- PHP files/postNewVaccination.php
+++ PHP files/postNewVaccination.php
@@ -10,20 +10,28 @@ mysql_connect("localhost", $username);
 
 $vaccination_id = $_GET["vaccination_id"];
 
+$patient_id = $_GET["patient_id"];
+
 $vaccine_id = $_GET["vaccine_id"];
-
-$date_taken = $_GET["date_taken"];
-
-$return_date = $_GET["return_date"];
 
 $physician_id = $_GET["physician_id"];
 
-$patient_id = $_GET["patient_id"];
+$date_taken = $_GET["date_taken"];
+
+$date_return = "";
 
 
-$query = "INSERT INTO vaccination VALUES ('$vaccination_id', '$vaccine_id', '$date_taken', '$return_date', '$physician_id', '$patient_id')";
+$query = "INSERT INTO vaccination VALUES ('$vaccination_id', '$patient_id', '$vaccine_id', '$physician_id', '$date_taken', '$date_return')";
 
-mysql_query($query) or die (mysql_error("error"));
+$result = mysql_query($query); //Result is Boolean type.
+
+if( $result ) {
+	echo "The new vaccination " . json_encode($vaccination_id) . " has been created Successfully! \n";	
+} else {
+	$errorMessage = "Cannot create new vaccination. Please check your connection or firewall.";
+	die ($errorMessage);
+}
+
 
 mysql_close();
 
